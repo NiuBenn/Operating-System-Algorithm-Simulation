a04c243b0e5966faf3681f8784520c2ca126dda9
NiuBenn <33975700+NiuBenn@users.noreply.github.com>
2018-11-28 15:45:12 +0800

    Corrected SJF's error algorithm

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"menu.h"
+
+int main()
+{
+	OSAlgorithmSimulation();
+	system("pause");
+	return 0;
+}
---
+++ menu.h
@@ -0,0 +1,58 @@
+#pragma once
+#include"OS.h"
+
+int menu_select()
+{
+	int i;
+	do {
+		system("cls");
+		std::cout << "\t\t ¨q¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ö¡õ¡ö¡õ¡ª¡ª¡ª¡ª¡ª¨r\n";
+		std::cout << "\t\t©¦     Operating-System-Algorithm-Simulation     ©¦\n";
+		std::cout << "\t\t¨t¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ö¡õ¡ö¡õ¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¨s\n";
+		std::cout << "\t\t   ©°¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª©´\n";
+		std::cout << "\t\t   ©¦         1. First Come First Served     ©¦\n";
+		std::cout << "\t\t   ©¦                                        ©¦\n";
+		std::cout << "\t\t   ©¦         2. Short Job First             ©¦\n";
+		std::cout << "\t\t   ©¦                                        ©¦\n";
+		std::cout << "\t\t   ©¦         3. Round Robin                 ©¦\n";
+		std::cout << "\t\t   ©¦                                        ©¦\n";
+		std::cout << "\t\t   ©¦         0. Exit                        ©¦\n";
+		std::cout << "\t\t   ©¸¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª¡ª©¼\n";
+		std::cout << "\t\tPlease choose(0-3):";
+		std::cin >> i;
+	} while (i < 0 || i>3);
+
+	return i;
+}
+
+void OSAlgorithmSimulation()
+{
+	std::vector<pcb> pcbs;
+	size_t roundtime;
+
+	while (1)
+	{
+		pcbs.clear();
+		switch (menu_select())
+		{
+		case 1:
+			GetPcbs(pcbs);
+			FCFS(pcbs);
+			break;
+		case 2:
+			GetPcbs(pcbs);
+			SJF(pcbs);
+			break;
+		case 3:
+			GetPcbs(pcbs);
+			GetRoundTime(roundtime);
+			RR(pcbs, roundtime);
+			break;
+		case 0:
+			std::cout << "\t\tsystem exit" << std::endl;
+			system("pause");
+			exit(0);
+		}
+	}
+}
+
