06e3d089f8b9a6f1fc841b75372eceedee1e6471
NiuBenn <33975700+NiuBenn@users.noreply.github.com>
2018-12-24 13:46:31 +0800

    Finish

--- Banker's_Algorithm/Banker.h
+++ Banker's_Algorithm/Banker.h
@@ -15,6 +15,7 @@ public:
 	std::vector<int> _Request;
 	size_t _PcbNum;
 	size_t _ResourcesNum;
+
 };
 
 void Init(Resources& R)
@@ -30,6 +31,7 @@ void Init(Resources& R)
 	R._Allocation.resize(R._PcbNum);
 	R._Need.resize(R._PcbNum);
 	R._MaxNeed.resize(R._PcbNum);
+
 	for (size_t i = 0; i < R._PcbNum; ++i)
 	{
 		R._Allocation[i].resize(R._ResourcesNum);
@@ -37,7 +39,7 @@ void Init(Resources& R)
 		R._MaxNeed[i].resize(R._ResourcesNum);
 	}
 	R._Work.resize(R._ResourcesNum);
-	R._Finish.resize(R._ResourcesNum);
+	R._Finish.resize(R._PcbNum);
 	R._Request.resize(R._ResourcesNum);
 
 	system("cls");
@@ -74,3 +76,169 @@ void Init(Resources& R)
 
 	system("pause");
 }
+
+void PrintResources(Resources& R)
+{	
+	printf("\n系统剩余资源：");
+	for (size_t i = 0; i < R._ResourcesNum; ++i)
+	{
+		printf("%4d", R._Available[i]);
+	}
+
+	printf("\n\n      目前占有量  最大需求量  尚需求量   \n");
+	for (size_t i = 0; i < R._PcbNum; ++i)
+	{
+		printf("P[%d]", i);
+		printf(" |");
+		for (size_t j = 0; j < R._ResourcesNum; ++j)
+		{
+			printf("%3d", R._Allocation[i][j]);
+		}
+		printf(" |");
+		for (size_t j = 0; j < R._ResourcesNum; ++j)
+		{
+			printf("%3d", R._MaxNeed[i][j]);
+		}
+		printf(" |");
+		for (size_t j = 0; j < R._ResourcesNum; ++j)
+		{
+			printf("%3d", R._Need[i][j]);
+		}
+		printf("\n");
+	}
+	system("pause");
+}
+
+bool IsSafety(Resources& R)
+{
+	for (size_t i = 0; i < R._PcbNum; ++i)
+	{
+		R._Finish[i] = false;
+	}
+
+	size_t FinshNum = 0;
+	int flag = 1;
+	printf("       Work      Need      Alloc    W+A     Finish\n");
+	while (flag)
+	{
+		flag = 0;
+		for (size_t i = 0; i < R._PcbNum; ++i)
+		{
+			if (R._Finish[i] == false)
+			{
+				int ResFlag = 1;
+				for (size_t j = 0; j < R._ResourcesNum; ++j)
+				{
+					if (R._Need[i][j] > R._Work[j])
+					{
+						ResFlag = 0;
+						break;
+					}
+				}
+				if (ResFlag)
+				{
+					R._Finish[i] = true;
+					flag = 1;
+					printf("P[%d]", i);
+					printf(" |");
+					for (size_t j = 0; j < R._ResourcesNum; ++j)
+					{
+						printf("%3d", R._Work[j]);
+					}
+					printf(" |");
+					for (size_t j = 0; j < R._ResourcesNum; ++j)
+					{
+						printf("%3d", R._Need[i][j]);
+					}
+					printf(" |");
+					for (size_t j = 0; j < R._ResourcesNum; ++j)
+					{
+						printf("%3d", R._Allocation[i][j]);
+					}
+					printf(" |");
+					for (size_t j = 0; j < R._ResourcesNum; ++j)
+					{
+						printf("%3d", R._Allocation[i][j] + R._Work[j]);
+						R._Work[j] += R._Allocation[i][j];
+					}
+					printf(" | true");
+					FinshNum++;
+					flag = 1;
+					printf("\n");
+				}
+			}
+		}
+	}
+
+	if (FinshNum < R._PcbNum)
+	{
+		std::cout << "不满足安全性算法!!" << std::endl;
+		return false;
+	}
+	else
+	{
+		std::cout << "满足安全性算法!!" << std::endl;
+		return true;
+	}
+
+}
+
+void Banker(Resources& R)
+{
+	system("cls");
+	
+	int PcbIndex;
+	int Flag = 1;
+	
+	while (Flag)
+	{
+		PrintResources(R);
+		std::cout << "请输入要进行资源分配的进程：";
+		std::cin >> PcbIndex;
+		for (size_t i = 0; i < R._ResourcesNum; ++i)
+		{
+			std::cout << "请输入对第" << i + 1 << "类资源的申请量：";
+			std::cin >> R._Request[i];
+			if (R._Request[i] > R._Need[PcbIndex][i])
+			{
+				std::cout << "申请资源失败：当前类资源申请量大于当前进程对该进程的最大需求数！！" << std::endl;
+				system("pause");
+				goto end;
+			}
+			if (R._Request[i] > R._Available[i])
+			{
+				std::cout << "申请资源失败：当前类资源申请量大于当前系统中该类资源总数量！！" << std::endl;
+				system("pause");
+				goto end;
+			}
+		}
+		for (size_t i = 0; i < R._ResourcesNum; ++i)
+		{
+			R._Available[i] -= R._Request[i];
+			R._Allocation[PcbIndex][i] += R._Request[i];
+			R._Need[PcbIndex][i] -= R._Request[i];
+			R._Work[i] = R._Available[i];
+		}
+
+		bool safe = IsSafety(R);
+		if (safe)
+		{
+			std::cout << "分配成功！！" << std::endl;
+		}
+		else
+		{
+			std::cout << "分配失败：当前状态不安全！！" << std::endl;
+			for (size_t i = 0; i < R._ResourcesNum; ++i)
+			{
+				R._Available[i] += R._Request[i];
+				R._Allocation[PcbIndex][i] -= R._Request[i];
+				R._Need[PcbIndex][i] += R._Request[i];
+			}
+		}
+end:
+		std::cout << "请选择：输入1继续分配，输入0退出分配：";
+		std::cin >> Flag;
+	}
+
+}
+
--- Banker's_Algorithm/main.cpp
+++ Banker's_Algorithm/main.cpp
@@ -8,14 +8,17 @@ int main()
 
 	while (1)
 	{
+		system("cls");
 		switch (menu_select())
 		{
 		case 1:
 			Init(R);
 			break;
 		case 2:
+			PrintResources(R);
 			break;
 		case 3:
+			Banker(R);
 			break;
 		case 0:
 			std::cout << "\t\tÏµÍ³ÍË³ö!" << std::endl;
